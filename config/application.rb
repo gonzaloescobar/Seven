5aeb01f4740383c843c83290ecb3af58b03d60ef
gonzaloescobar <gonzalo.david.escobar@gmail.com>
2017-09-09 21:25:04 -0300

    Heroku icons fix

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Seven
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{Rails.root}/lib/validators)
+	config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+	config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'bootstrap-sass-official')
+	config.assets.precompile += %W( *.eot *.svg *.ttf *.woff *.woff2 *.otf )
   end
 end
